602f01a79533f102697ceda241afdad7c5ac1f78
Ujjwal Tyagi <ujjwaltyagi9605@gmail.com>
2025-07-16 01:08:04 +0530

    final submission of event management api

--- src/controller/event_controller.js
+++ src/controller/event_controller.js
@@ -1,78 +1,80 @@
-const pool = require("../connection/database")
+const pool = require("../connection/database");
 
-const createEvent = async (req,res)=>{
-    try {
-    const {title , Event_Location, Event_Time , Capacity} = req.body;
+const createEvent = async (req, res) => {
+  try {
+    const { title, Event_Location, Event_Time, Capacity } = req.body;
 
-    if (Capacity>1000) {
-        return res.status(400).json({
-            success : false,
-            error : 'exceed max capacity limit(1000)'
-        })
+    if (Capacity > 1000) {
+      return res.status(400).json({
+        success: false,
+        error: "exceed max capacity limit(1000)",
+      });
     }
     const event = await pool.query(
-        "INSERT INTO Event_Detail (title , Event_Location, Event_Time , Capacity) VALUES ($1, $2, $3, $4) RETURNING *",
-        [title , Event_Location, Event_Time , Capacity]
-    )
-        return res.status(200).json({
-            success : true,
-            data : event.rows[0],
-            error : null
-        })
-    } catch (error) {
-        console.log(`error in creating event ${error.message}`)
-        return res.status(500).json({
-            success : false,
-            data : null,
-            error :error.message
-        })
+      "INSERT INTO Event_Detail (title , Event_Location, Event_Time , Capacity) VALUES ($1, $2, $3, $4) RETURNING *",
+      [title, Event_Location, Event_Time, Capacity]
+    );
+    return res.status(200).json({
+      success: true,
+      data: event.rows[0],
+      error: null,
+    });
+  } catch (error) {
+    console.log(`error in creating event ${error.message}`);
+    return res.status(500).json({
+      success: false,
+      data: null,
+      error: error.message,
+    });
+  }
+};
+
+const deleteEvent = async (req, res) => {
+  try {
+    await pool.query("DELETE FROM Event_Detail WHERE Unique_Id = $1", [
+      req.params.id,
+    ]);
+    res.json({ message: "Event deleted" });
+  } catch (error) {
+    console.log(`Event deletion failed ${error.message}`);
+    return res.status(500).json({
+      success: false,
+      data: null,
+      error: error.message,
+    });
+  }
+};
+
+const getEvent = async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * from Event_Detail");
+    if (result.rows.length === 0) {
+      console.log("Event not found");
+      return res.status(400).json({
+        success: false,
+        error: "user not found",
+      });
     }
-}
+    return res.status(200).json({
+      success: true,
+      data: result.rows,
+      error: null,
+    });
+  } catch (error) {
+    console.log(`error in fetching event ${error.message}`);
+    return res.status(500).json({
+      success: false,
+      data: null,
+      error: error.message,
+    });
+  }
+};
 
-const deleteEvent = async (req,res)=>{
-    try {
-        await pool.query("DELETE FROM Event_Detail WHERE Unique_Id = $1", [req.params.id]);
-        res.json({ message: "Event deleted" });
-    } catch (error) {
-        console.log(`Event deletion failed ${error.message}`)
-        return res.status(500).json({
-            success : false,
-            data : null,
-            error :error.message
-        })
-    }
-}
+const Register = async (req, res) => {
+  try {
+    const { event_id, user_id } = req.body;
 
-const getEvent = async (req,res)=>{
-    try {
-        const result = await pool.query("SELECT * from Event_Detail") ;
-        if(result.rows.length === 0) {
-            console.log('Event not found');
-            return res.status(400).json({
-            success : false,
-            error : 'user not found'
-        })
-        }
-        return res.status(200).json({
-            success : true,
-            data : result.rows,
-            error : null
-        })
-    } catch (error) {
-        console.log(`error in fetching event ${error.message}`)
-        return res.status(500).json({
-            success : false,
-            data : null,
-            error :error.message
-        })
-    }
-}
-
-const Register = async (req ,res)=>{
-    try {
-        const {event_id,user_id} = req.body;
-
-        const eventdetail = await pool.query(
+    const eventdetail = await pool.query(
       "SELECT * FROM Event_Detail WHERE Unique_Id = $1",
       [event_id]
     );
@@ -105,83 +107,87 @@ const Register = async (req ,res)=>{
       });
     }
 
-        const result = await pool.query(
-            "INSERT INTO Registrations (event_id,user_id) VALUES ($1, $2) RETURNING *",
-            [event_id,user_id]
-        )
-        return res.status(200).json({
-            success : true,
-            data : result.rows[0],
-            error : null
-        })
-    } catch (error) {
-        console.log(`User registration failed event ${error.message}`)
-        return res.status(500).json({
-            success : false,
-            data : null,
-            error :error.message
-        })
-    }
-}
-const getAllRegistration = async (req,res)=>{
-    try {
-        const result = await pool.query("SELECT * from Registrations") ;
-        if(result.rows.length === 0) {
-            console.log('Registrations not found');
-            return res.status(400).json({
-            success : false,
-            error : 'Registrations not found'
-        })
-        }
-        return res.status(200).json({
-            success : true,
-            data : result.rows,
-            error : null
-        })
-    } catch (error) {
-        console.log(`Registration fetching failed event ${error.message}`)
-        return res.status(500).json({
-            success : false,
-            data : null,
-            error :error.message
-        })
-    }
-}
-
-const cancelRegistration = async (req,res)=>{
-    try {
-        await pool.query("DELETE FROM Registrations WHERE Unique_Id = $1", [req.params.id]);
-        res.json({ message: "Registration deleted" });
-    } catch (error) {
-        console.log(`Registration cancel failed event ${error.message}`)
-        return res.status(500).json({
-            success : false,
-            data : null,
-            error :error.message
-        })
-    }
-}
-
-const upcomingEvent = async (req ,res)=>{
-    try {
-        const result = await pool.query("SELECT * FROM Event_Detail WHERE Event_Time > NOW() ORDER BY Event_Time ASC");
-        return res.status(200).json({
-        success: true,
-        data: result.rows,
+    const result = await pool.query(
+      "INSERT INTO Registrations (event_id,user_id) VALUES ($1, $2) RETURNING *",
+      [event_id, user_id]
+    );
+    return res.status(200).json({
+      success: true,
+      data: result.rows[0],
+      error: null,
     });
-    } catch (error) {
-        console.log(`Upcoming event fetch failed ${error.message}`)
-        return res.status(500).json({
-            success : false,
-            data : null,
-            error :error.message
-        })
+  } catch (error) {
+    console.log(`User registration failed event ${error.message}`);
+    return res.status(500).json({
+      success: false,
+      data: null,
+      error: error.message,
+    });
+  }
+};
+const getAllRegistration = async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * from Registrations");
+    if (result.rows.length === 0) {
+      console.log("Registrations not found");
+      return res.status(400).json({
+        success: false,
+        error: "Registrations not found",
+      });
     }
-}
+    return res.status(200).json({
+      success: true,
+      data: result.rows,
+      error: null,
+    });
+  } catch (error) {
+    console.log(`Registration fetching failed event ${error.message}`);
+    return res.status(500).json({
+      success: false,
+      data: null,
+      error: error.message,
+    });
+  }
+};
 
-const eventStats = async (req,res) =>{
-    try {
-        const result = await pool.query(`
+const cancelRegistration = async (req, res) => {
+  try {
+    await pool.query("DELETE FROM Registrations WHERE Unique_Id = $1", [
+      req.params.id,
+    ]);
+    res.json({ message: "Registration deleted" });
+  } catch (error) {
+    console.log(`Registration cancel failed event ${error.message}`);
+    return res.status(500).json({
+      success: false,
+      data: null,
+      error: error.message,
+    });
+  }
+};
+
+const upcomingEvent = async (req, res) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM Event_Detail WHERE Event_Time > NOW() ORDER BY Event_Time ASC"
+    );
+    return res.status(200).json({
+      success: true,
+      data: result.rows,
+    });
+  } catch (error) {
+    console.log(`Upcoming event fetch failed ${error.message}`);
+    return res.status(500).json({
+      success: false,
+      data: null,
+      error: error.message,
+    });
+  }
+};
+
+const eventStats = async (req, res) => {
+  try {
+    const result = await pool.query(`
       SELECT 
         e.Unique_Id AS event_id,
         e.Title,
@@ -199,13 +205,22 @@ const eventStats = async (req,res) =>{
       success: true,
       data: result.rows,
     });
-    } catch (error) {
-        console.log(`eventStats fetch failed ${error.message}`)
-        return res.status(500).json({
-            success : false,
-            data : null,
-            error :error.message
-        })
-    }
-}
-module.exports = {createEvent,deleteEvent,getEvent,Register,getAllRegistration,cancelRegistration,upcomingEvent,eventStats};
+  } catch (error) {
+    console.log(`eventStats fetch failed ${error.message}`);
+    return res.status(500).json({
+      success: false,
+      data: null,
+      error: error.message,
+    });
+  }
+};
+module.exports = {
+  createEvent,
+  deleteEvent,
+  getEvent,
+  Register,
+  getAllRegistration,
+  cancelRegistration,
+  upcomingEvent,
+  eventStats,
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-const express = require('express');
-const eventRouter = require('./routes/event_routes');
-const userRouter = require('./routes/user_routes');
-const dotenv = require('dotenv');
+const express = require("express");
+const eventRouter = require("./routes/event_routes");
+const userRouter = require("./routes/user_routes");
+const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use('/api/event',eventRouter);
-app.use('/api/user',userRouter);
+app.use("/api/event", eventRouter);
+app.use("/api/user", userRouter);
 const PORT = process.env.PORT;
-app.listen(PORT,()=>{
-    console.log(`server started at PORT ${PORT}`);
-})
-
-
-
+app.listen(PORT, () => {
+  console.log(`server started at PORT ${PORT}`);
+});
